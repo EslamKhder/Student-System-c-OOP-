c0dd5f057e6cc2a994d1a870cd04170caa877add
eslam khder <eslamkhder11@gmail.com>
2023-01-10 15:58:12 +0200

    Create Model Student

---
+++ model/Student.cpp
@@ -0,0 +1,42 @@
+class Student {
+	private:
+		int id;
+		string name;
+		int age;
+		string phoneNumber;
+		double gpa;
+	public:
+		//  Setter
+		void setId(int id){
+			this->id = id;
+		}
+		void setName(string name){
+			this->name = name;
+		}
+		void setAge(int age){
+			this->age = age;
+		}
+		void setPhoneNumber(string phoneNumber){
+			this->phoneNumber = phoneNumber;
+		}
+		void setGpa(double gpa){
+			this->gpa = gpa;
+		}
+		
+		// Getter
+		int getId(){
+			return id;
+		}
+		string getName(){
+			return name;
+		}
+		int getAge(){
+			return age;
+		}
+		string getPhoneNumber(){
+			return phoneNumber;
+		}
+		double getGpa(){
+			return gpa;
+		}
+};
