c5fefe4b4ef780d2026a5136150b6577ac89819c
eslam khder <eslamkhder11@gmail.com>
2023-01-17 23:09:43 +0200

    Repo(Student - Teacher - Course)

--- model/Repository.cpp
+++ model/Repository.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+/////////////////////////////  Student \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// interface StudentRepository
+class StudentRepository {
+	
+};
+
+// Class StudentRepositoryImpl
+class StudentRepositoryImpl: public StudentRepository {
+	
+};
+
+/////////////////////////////  Course \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// interface CourseRepository
+class CourseRepository {
+	
+};
+
+// Class CourseRepositoryImpl
+class CourseRepositoryImpl: public CourseRepository {
+	
+};
+
+/////////////////////////////  Teacher \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// interface TeacherRepository
+class TeacherRepository {
+	
+};
+
+// Class TeacherRepositoryImpl
+class TeacherRepositoryImpl: public TeachertRepository {
+	
+};
