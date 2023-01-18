ecf3dac8ef82b7c441d3e1073e20e4e8c2e070f0
eslam khder <eslamkhder11@gmail.com>
2023-01-18 19:01:26 +0200

    Impl StudentRepo addStudent

--- repository/Repository.cpp
+++ repository/Repository.cpp
@@ -1,15 +1,29 @@
+#include "../model/Models.cpp"
 #include<bits/stdc++.h>
 using namespace std;
 
 /////////////////////////////  Student \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // interface StudentRepository
 class StudentRepository {
-	
+	public:
+		virtual int addStudent(Student student);
+		
 };
 
 // Class StudentRepositoryImpl
 class StudentRepositoryImpl: public StudentRepository {
-	
+	private:
+		Student students[25];
+		int index = 0;
+	public:
+		int addStudent(Student student) {
+			if(index == 25){
+				cout<<"Full Student"<<endl;
+			} else {
+				students[index] = student;
+				index++;
+			}
+		}
 };
 
 /////////////////////////////  Course \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -30,6 +44,6 @@ class TeacherRepository {
 };
 
 // Class TeacherRepositoryImpl
-class TeacherRepositoryImpl: public TeachertRepository {
+class TeacherRepositoryImpl: public TeacherRepository {
 	
 };
