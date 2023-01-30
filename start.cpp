cd2c174b99058723eb1a5ab8f92e97e2b9358d58
Eslam.khder <eslamkhder11@gmail.com>
2023-01-30 16:54:31 +0200

    add student Part 1

--- start.cpp
+++ start.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include "model/Models.cpp"
 using namespace std;
 void displaySystem(){
-	cout<<"\t\t************ Student Management System ************"<<endl;
+	cout<<"\t\t************ Management System ************"<<endl;
 	cout<<"Please Enter Your Process you need to do !"<<endl;
 	cout<<"1 - About Students"<<"\t\t"<<"2 - About Courses"<<endl;
 	cout<<"3 - About Teachers"<<"\t\t"<<"4 - Exit"<<endl;
 }
 void showList(string value){
+	cout<<"\t\t************ "<<value<<" Management System ************"<<endl;
 	cout<<"1 - Add "<<value<<"\t\t"<<"2 - Remove "<<value<<endl;
 	cout<<"3 - Edit "<<value<<"\t"<<"4 - Show "<<value<<endl;
 	cout<<"5 - Exit"<<endl;
@@ -20,6 +22,30 @@ int main(){
 		switch(process){
 			case 1:
 				showList("Student");
+				int processStudent;
+				cin>>processStudent;
+				if(processStudent == 1){
+					cout<<"Please Enter Student Data: "<<endl;
+					Student student;
+					cout<<"Enter Student Name :";
+					string name;
+					cin>>name;
+					student.setName(name);
+					cout<<"Enter Student Age :";
+					int age;
+					cin>>age;
+					student.setAge(age);
+					cout<<"Enter Student PhoneNumber :";
+					string phoneNumber;
+					cin>>phoneNumber;
+					student.setPhoneNumber(phoneNumber);
+					cout<<"Enter Student GPA :";
+					double gpa;
+					cin>>gpa;
+					student.setGpa(gpa);
+					
+					cout<<student.getName()<<" "<<student.getPhoneNumber()<<"  "<<student.getAge()<<"  "<<student.getGpa()<<endl;
+				}
 				break;
 			case 2:
 				showList("Course");
