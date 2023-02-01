bb48002678c964f3d7f08340302a0b372bf0bc2c
Eslam.khder <eslamkhder11@gmail.com>
2023-02-02 00:44:26 +0200

    updateStudent - updateCourse - updateTeacher (Controller)

--- controller/Controller.cpp
+++ controller/Controller.cpp
@@ -17,6 +17,9 @@ class StudentController {
 				cout<<"\t\t"<<studentResult.getId()<<"\t\t"<<studentResult.getName()<<"\t\t"<<studentResult.getAge()<<"\t\t"<<studentResult.getPhoneNumber()<<"\t\t"<<studentResult.getGpa()<<endl;
 			}
 		}
+		void updateStudent(Student student){
+			studentService.editStudent(student);
+		}
 };
 /////////////////////////////  Course \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // Class CourseController
@@ -35,6 +38,9 @@ class CourseController {
 				cout<<"\t\t"<<courseResult.getId()<<"\t\t"<<courseResult.getName()<<"\t\t"<<courseResult.getHour()<<endl;
 			}
 		}
+		void updateCourse(Course course){
+			courseService.editCourse(course);
+		}
 };
 /////////////////////////////  Teacher \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // Class TeacherController
@@ -52,5 +58,8 @@ class TeacherController {
 				cout<<"\t\t ID \t\t Name \t\t Age \t\t Phone Number \t\t Salary"<<endl;
 				cout<<"\t\t"<<teacherResult.getId()<<"\t\t"<<teacherResult.getName()<<"\t\t"<<teacherResult.getAge()<<"\t\t"<<teacherResult.getPhoneNumber()<<"\t\t"<<teacherResult.getSalary()<<endl;
 			}
+		}
+		void updateTeacher(Teacher teacher){
+			teacherService.editTeacher(teacher);
 		}
 };
